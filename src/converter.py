3862ab39cbde0cefd0e436b4813f2d600b86880f
Aaron Berger <aaronzberger@gmail.com>
2022-06-27 11:57:36 -0400

    Add action clients

--- src/converter.py
+++ src/converter.py
@@ -7,11 +7,22 @@ import rospy
 from interval_tree import IntervalTree
 
 
+def field_type(str):
+    return {
+        'float': type(0.),
+        'str': type(''),
+        'int': type(0)
+    }[str]
+
+
 @dataclass
 class Constraint():
     'Represent a constraint on an action, relative to another action'
-    constraint: Literal["before", "after"]
+    constraint: Literal['before', 'after']
     action: str
+
+    def __eq__(self, other):
+        return self.constraint == other.constraint and self.action == other.action
 
 
 @dataclass
@@ -36,8 +47,8 @@ class Action():
 class Converter:
     def __init__(self, plan_file: str):
         self.plan_file = plan_file
-        self.actions = []
         self.interval_tree = None
+
         self.mappings = {
             'robot': 'robot',
             'start-location': 'start',
@@ -51,17 +62,21 @@ class Converter:
             'location': '',
             'gripper-status': ''
         }
-        # print(type(Action().__dataclass_fields__['robot'].type))
         self.features = []
+
+        self.actions = []
+        self.action_table = {}
 
     def register_action(self, action_text: str):
         # First, add the action to the list of running actions
         args = []
+
         for item in action_text[1:]:
             name = self.mappings[item[:item.find(':')]]
             value = item[item.find(':') + 2:].replace('\'', '')
             if name != 'constraints' and name != '':
-                value = (type(Action().__dataclass_fields__[name].type))(value)
+                # Cast the value to the correct type according to Action's definition
+                value = (field_type(Action().__dataclass_fields__[name].type))(value)
             else:
                 value = []
 
@@ -101,7 +116,7 @@ class Converter:
             idx = text.find(';')
             return text.strip() if idx == -1 else text[:idx].strip()
 
-        file = open(self.plan_file, "r")
+        file = open(self.plan_file, 'r')
         action_text = []
         paren_stack = 0
         for line in (clean_line(line) for line in file.readlines() if len(clean_line(line)) != 0):
@@ -115,10 +130,30 @@ class Converter:
             if paren_stack == 0 and len(action_text) != 0:
                 self.register_action(combine_constraints(action_text=action_text))
                 action_text = []
+
         self.timeline = IntervalTree(self.features, 0, max([i[1] for i in self.features]))
 
-        rospy.loginfo('Registered %d actions successfully.', len(self.actions))
+        # Put all the actions into a hash table
+        for action in self.actions:
+            self.action_table[action.name] = action
+
+        # Equalize the constraints: ensure they are bi-directional
+        for action in self.actions:
+            for constraint in action.constraints:
+                reciprocal_constraint = Constraint(
+                    constraint='after' if constraint.constraint == 'before' else 'before', action=action.name)
+                if reciprocal_constraint not in self.action_table[constraint.action].constraints:
+                    self.action_table[constraint.action].constraints.append(reciprocal_constraint)
+
+        # We are now done using the temporary self.actions variable
+        print(self.actions)
+        del self.actions
+
+        rospy.loginfo('Registered %d actions successfully.', len(self.action_table))
         rospy.loginfo('Created interval tree with %d nodes successfully.', len(self.features))
 
     def get_timeline(self):
         return self.timeline
+
+    def get_action_table(self):
+        return self.action_table
--- src/main.py
+++ src/main.py
@@ -3,7 +3,19 @@
 import rospy
 import sys
 import os
-from converter import Converter
+from converter import Converter, Action
+
+import actionlib
+import plan2sim.msg as msg
+
+
+def server(action: Action):
+    if action.robot == 'UR5A':
+        return 'ur5a_rail' if 'rail' in action.name else 'ur5a_arm'
+    if action.robot == 'UR5B':
+        return 'ur5b_rail' if 'rail' in action.name else 'ur5b_arm'
+    if action.robot == 'FF1':
+        return 'free_flyer' if 'traverse' in action.name else 'free_flyer_arm'
 
 
 class Plan2Sim:
@@ -11,6 +23,62 @@ class Plan2Sim:
         self.converter_node = Converter(plan_file)
         self.converter_node.convert()
         self.interval_tree = self.converter_node.get_timeline()
+        self.action_table = self.converter_node.get_action_table()
+
+        self.constraints = []
+
+        self.action_clients = {
+            'ur5a_arm': actionlib.SimpleActionClient('ur5a_arm', msg.PerformTaskAction),
+            'ur5a_rail': actionlib.SimpleActionClient('ur5a_rail', msg.PerformTaskAction),
+            'ur5b_arm': actionlib.SimpleActionClient('ur5b_arm', msg.PerformTaskAction),
+            'ur5b_rail': actionlib.SimpleActionClient('ur5b_rail', msg.PerformTaskAction),
+            'free_flyer': actionlib.SimpleActionClient('free_flyer', msg.PerformTaskAction),
+            'free_flyer_arm': actionlib.SimpleActionClient('free_flyer', msg.PerformTaskAction)
+        }
+
+        # for i, client in enumerate(self.action_clients):
+        #     print('Waiting for {} server ({}/{})... '.format(
+        #         client, i + 1, len(self.action_clients)), end='', flush=True)
+        #     self.action_clients[client].wait_for_server()
+        #     print('CONNECTED')
+        rospy.loginfo('All servers connected.')
+
+        # Begin the planner
+        self.actions_completed = set()
+        self.actions_executing = set()
+        self.start_time = rospy.get_rostime()
+        rospy.Timer(rospy.Duration(secs=1), self.planner)
+
+    def planner(self, event: rospy.timer.TimerEvent):
+        def run(action: Action):
+            goal = msg.PerformTaskGoal(task=msg.action(
+                name=action.name, start_time=action.start_time, end_time=action.end_time,
+                start=action.start, end=action.end))
+
+            # Send the goal to the server and register the feedback and results callbacks
+            client = server(action)
+            if client:
+                self.action_clients[client].send_goal(
+                    goal, feedback_cb=(lambda fb: print('{} action is {}% done'.format(action.name, fb.percent_complete))),
+                    done_cb=(lambda _, res: print(
+                        '{} completed at {} seconds at position {}'.format(
+                            action.name, res.result.time_ended, res.result.final_position))))
+            else:
+                print('Unknown implementation of action {}'.format(action.name))
+            # self.action_clients[action.name].wait_for_result(rospy.Duration.from_sec(5))
+
+        if event.last_real is None:
+            event.last_real = self.start_time
+
+        # Get the actions that should be running now from the interval tree
+        current_actions = self.interval_tree.find_range(
+            [(event.last_real - self.start_time).to_sec(), (event.current_real - self.start_time).to_sec()])
+
+        for action in current_actions:
+            if action not in self.actions_completed | self.actions_executing:
+
+                # Deal with all constraints
+                run(self.action_table[action])
 
 
 if __name__ == '__main__':
